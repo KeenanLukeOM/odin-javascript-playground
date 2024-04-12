a830605c475523bad80ee2092aae35c73cd1385a
KeenanLukeOM <76129769+KeenanLukeOM@users.noreply.github.com>
2024-04-12 02:08:30 -0700

    Learning const

--- javascript.js
+++ javascript.js
@@ -1,12 +1,21 @@
-let hello = "Hello world!";
+"use strict";
 
-let message;
+const myBirthday = "03.20.2000";
 
-message = hello;
+myBirthday = "30.02.0002"; //This causes an error, const are unchanging variables
 
-alert(hello);
-alert(message);
+/////////
 
+// let hello = "Hello world!";
+
+// let message;
+
+// message = hello;
+
+// alert(hello);
+// alert(message);
+
+////////
 
 // let message = "Hello";
 
