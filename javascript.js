7a8b0f9bad9c293dbfcdb8f115090ada8181dc4b
KeenanLukeOM <76129769+KeenanLukeOM@users.noreply.github.com>
2024-04-16 02:11:09 -0700

    ODIN fizzbuzz demonstration

--- javascript.js
+++ javascript.js
@@ -1,10 +1,25 @@
 "use strict";
 
-function lastLetter(text) {
-    return text.substr(-1)
-}
+let answer = parseInt(prompt("Please enter the number you would like to FizzBuzz up to: "));
 
-console.log(lastLetter("Hello"))
+for (let i = 1; i <= answer; i++) {
+    if (i % 3 === 0 && i % 5 === 0) {
+      console.log("FizzBuzz");
+    } else if (i % 3 === 0) {
+      console.log("Fizz");
+    } else if (i % 5 === 0) {
+      console.log("Buzz");
+    } else {
+      console.log(i);
+    }
+  }
+
+
+// function lastLetter(text) {
+//     return text.substr(-1)
+// }
+
+// console.log(lastLetter("Hello"))
 
 // function capitaliize(lowercase) {
 //     let firstLetter = lowercase.substring(0,1).toUpperCase()
